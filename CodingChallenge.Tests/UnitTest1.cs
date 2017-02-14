8ba7a489d2e9ddec75c5610a561919af02d1f41a
ErRoen <errbase-gh@yahoo.com>
2017-02-13 19:43:40 -0600

    Refactored unit tests a bit

--- CodingChallenge.Tests/UnitTest1.cs
+++ CodingChallenge.Tests/UnitTest1.cs
@@ -14,28 +14,33 @@ namespace CodingChallenge.Tests.PaycheckTestContainer
         [TestClass]
         public class PaycheckAmountMethod : PaycheckTests
         {
-            [TestMethod]
-            public void EmployeeSolo()
+            private static void BaseTest(Employee employee, decimal amount)
             {
-                var something = new Something(new Employee("Erick", GrossWagesPerPayPeriod));
+                var something = new Something(employee);
                 var paycheckAmount = something.GetPaycheckAmount();
 
-                var expected = new Currency(1961.538461538462M).Amount;
+                var expected = new Currency(amount).Amount;
                 var actual = new Currency(paycheckAmount).Amount;
 
                 Assert.AreEqual(expected, actual);
             }
 
             [TestMethod]
+            public void EmployeeSolo()
+            {
+                const decimal amount = 1961.538461538462M;
+                var employee = new Employee("Erick", GrossWagesPerPayPeriod);
+
+                BaseTest(employee, amount);
+            }
+
+            [TestMethod]
             public void EmployeeSoloWithAName()
             {
-                var something = new Something(new Employee("Atticus", GrossWagesPerPayPeriod));
-                var paycheckAmount = something.GetPaycheckAmount();
+                const decimal amount = 1965.384615384615M;
+                var employee = new Employee("Atticus", GrossWagesPerPayPeriod);
 
-                var expected = new Currency(1965.384615384615M).Amount;
-                var actual = new Currency(paycheckAmount).Amount;
-
-                Assert.AreEqual(expected, actual);
+                BaseTest(employee, amount);
             }
         }
     }
